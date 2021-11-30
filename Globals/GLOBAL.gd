18aa583456bf6e54ae4c6c1d11eb28240f5464a0
daxter154 <justinmwinningham@gmail.com>
2021-11-29 22:56:21 -0800

    Refactor Map Loading
    Added MAPCUSTODIAN to handle validating and loading maps into memory at launch
    
    Added Map class (Map.gd) for holding map data
    Refactored MapBuilder (previously Map.gd) to work with new MAPCUSTODIAN architecture
    
    Added GLOBAL autoload file for global methods outside the scope of MAPCUSTODIAN
    
    Added second testing map
    
    TODO: Validate the data (faking it right now) and cleanup

--- Globals/GLOBAL.gd
+++ Globals/GLOBAL.gd
@@ -1,12 +1,17 @@
 extends Node
 
-var mapToLoad = "res://Maps/testmap"
-
 func _ready():
 	pass # Replace with function body.
 
-func setMap(map):
-	mapToLoad = map
-
-func _getMap():
-	return mapToLoad
+func parseJSON(file):
+	var text = file.get_as_text()
+	var result_json = JSON.parse(text)
+	if result_json.error == OK: # if parse doesn't fail
+		var data = result_json.result
+		return data
+	else:
+		print_debug("Failed to parse JSON", file.filename)
+		print_debug("Error: ", result_json.error)
+		print_debug("Error Line: ", result_json.error_line)
+		print_debug("Error String: ", result_json.error_string)
+		return null
--- Testing/Map.gd
+++ Testing/Map.gd
@@ -1,61 +1,34 @@
-extends Node2D
-
-var mapPath = null
+# This is a class for maps and all data related to them - namely terrain
+var mapPath
 var terrainData = []
 var mapName
 var mapAuthor
 var mapSizeX
 var mapSizeY
-var tilesetName
+var tileSet
 var mapDataDict = {}
 
-var tileSet = 'res://Testing/PlaceholderTileSet.tres'
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	mapPath = GLOBAL._getMap() # get the exact directory to load map from
+func _init(path):
+	self.mapPath = path
 	var file = File.new()
-	file.open(mapPath + "/mapData.json", file.READ)
-	var text = file.get_as_text()
-	var result_json = JSON.parse(text)
-	if result_json.error == OK: # if parse doesn't fail
-		var data = result_json.result
-		print_debug("Successful mapData parse!")
-		mapDataDict = data
-	else:
-		print_debug("Failed to parse mapData for " + mapPath + "/mapData.json")
-		print_debug("Error: ", result_json.error)
-		print_debug("Error Line: ", result_json.error_line)
-		print_debug("Error String: ", result_json.error_string)
+	file.open(path + "/mapData.json", file.READ)
+	self.mapDataDict = GLOBAL.parseJSON(file)
 	file.close()
-	mapName = mapDataDict["mapName"]
-	mapAuthor = mapDataDict["author"]
-	self.get_node("DebugLabel").set_text(mapName + " by " + mapAuthor)
+	self.mapName = mapDataDict["mapName"]
+	self.mapAuthor = mapDataDict["author"]
+	self.tileSet = load("res://Tilesets/" + mapDataDict["tileset"])
+	# Left off setting the mode to atlas - dont think it worked previously anyway
 	
-	file.open(mapPath + "/terrainData.csv", file.READ)
-	text = file.get_as_text()
-	file.close()
-	# TODO: Make more expandable. This approach is going to be a problem if we want more than 10 tile types
+	file.open(path + "/terrainData.csv", file.READ)
+	var text = file.get_as_text()
 	var newtext = text.replace(',','')
-	print_debug(newtext)
-	var row = []
+	var row= []
 	for c in newtext:
 		if c != '\n':
 			row.append(c)
 		else:
 			terrainData.append(row)
 			row = []
-	print_debug(terrainData)
-	pass # Replace with function body.
+	self.mapSizeX = terrainData.size()
+	self.mapSizeY = terrainData[1].size()
 	
-	mapSizeX = terrainData.size()
-	mapSizeY = terrainData[1].size()
-	print_debug(mapSizeX)
-	print_debug(mapSizeY)
-	
-	$TileMap.tile_set = load('res://Testing/PlaceholderTileSet.tres')
-	$TileMap.tile_set.tile_set_tile_mode(2,TileSet.ATLAS_TILE)
-	
-	for i in mapSizeX:
-		for j in mapSizeY:
-			$TileMap.set_cell(i+2, j+2, int(terrainData[j][i]) - 1) # formatting adjustment 1 vs 0 start
